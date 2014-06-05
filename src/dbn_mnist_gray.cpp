dd1681ba04c814be358b6f5090f62e34abc8a8e5
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-06-05 08:17:47 +0200

    Read the dataset completely once

--- src/dbn_mnist_gray.cpp
+++ src/dbn_mnist_gray.cpp
@@ -19,35 +19,33 @@
 namespace {
 
 template<typename Container>
-void normalize_each(Container& values){
+void scale_each(Container& values){
     for(auto& vec : values){
         for(auto& v : vec){
             v /= 255.0;
-//            v = v > 30.0 ? 1.0 : 0.0;
         }
     }
 }
 
-template<typename DBN, typename P>
-void test_all(DBN& dbn, std::vector<vector<double>>& training_images, const std::vector<uint8_t>& training_labels, P&& predictor){
-    auto test_images = mnist::read_test_images<std::vector, vector, double>();
-    auto test_labels = mnist::read_test_labels<std::vector>();
-
-    normalize_each(test_images);
-
-    if(test_images.empty() || test_labels.empty()){
-        std::cout << "Impossible to read test set" << std::endl;
-        return;
+template<typename Container>
+void binarize_each(Container& values, double threshold = 30.0){
+    for(auto& vec : values){
+        for(auto& v : vec){
+            v = v > threshold ? 1.0 : 0.0;
+        }
     }
+}
 
+template<typename DBN, typename Dataset, typename P>
+void test_all(DBN& dbn, Dataset& dataset, P&& predictor){
     std::cout << "Start testing" << std::endl;
 
-    std::cout << "Training Set (" << training_images.size() << ")" << std::endl;
-    auto error_rate = dbn::test_set(dbn, training_images, training_labels, predictor);
+    std::cout << "Training Set (" << dataset.training_images.size() << ")" << std::endl;
+    auto error_rate = dbn::test_set(dbn, dataset.training_images, dataset.training_labels, predictor);
     std::cout << "\tError rate (normal): " << 100.0 * error_rate << std::endl;
 
-    std::cout << "Test Set (" << test_images.size() << ")" << std::endl;
-    error_rate =  dbn::test_set(dbn, test_images, test_labels, predictor);
+    std::cout << "Test Set (" << dataset.test_images.size() << ")" << std::endl;
+    error_rate =  dbn::test_set(dbn, dataset.test_images, dataset.test_labels, predictor);
     std::cout << "\tError rate (normal): " << 100.0 * error_rate << std::endl;
 }
 
@@ -94,14 +92,15 @@ int main(int argc, char* argv[]){
         }
     }
 
-    auto training_images = mnist::read_training_images<std::vector, vector, double>();
-    auto training_labels = mnist::read_training_labels<std::vector>();
+    auto dataset = mnist::read_dataset<std::vector, vector, double>();
 
-    if(training_images.empty() || training_labels.empty()){
+    if(dataset.training_images.empty() || dataset.training_labels.empty()){
+        std::cout << "Impossible to read dataset" << std::endl;
         return 1;
     }
 
-    normalize_each(training_images);
+    scale_each(dataset.training_images);
+    scale_each(dataset.test_images);
 
     if(simple){
         typedef dbn::dbn<
@@ -111,9 +110,9 @@ int main(int argc, char* argv[]){
 
         auto dbn = std::make_shared<dbn_simple_t>();
 
-        dbn->train_with_labels(training_images, training_labels, 10, 5);
+        dbn->train_with_labels(dataset.training_images, dataset.training_labels, 10, 5);
 
-        test_all(dbn, training_images, training_labels, dbn::label_predictor());
+        test_all(dbn, dataset, dbn::label_predictor());
     } else {
         typedef dbn::dbn<
             dbn::layer<dbn::conf<true, 100, true, true>, 28 * 28, 500>,
@@ -121,7 +120,7 @@ int main(int argc, char* argv[]){
             dbn::layer<dbn::conf<true, 100, false, true>, 500, 2000>,
             dbn::layer<dbn::conf<true, 100, false, true, true, dbn::Type::SOFTMAX>, 2000, 10>> dbn_t;
 
-        auto labels = dbn::make_fake(training_labels);
+        auto labels = dbn::make_fake(dataset.training_labels);
 
         auto dbn = std::make_unique<dbn_t>();
 
@@ -134,24 +133,24 @@ int main(int argc, char* argv[]){
             dbn->load(is);
         } else {
             std::cout << "Start pretraining" << std::endl;
-            dbn->pretrain(training_images, 5);
+            dbn->pretrain(dataset.training_images, 5);
 
             std::cout << "Start fine-tuning" << std::endl;
-            dbn->fine_tune(training_images, labels, 5, 1000);
+            dbn->fine_tune(dataset.training_images, labels, 5, 1000);
 
             std::ofstream os("dbn_gray.dat", std::ofstream::binary);
             dbn->store(os);
         }
 
         if(prob){
-            display(dbn, training_images[256]);
-            display(dbn, training_images[512]);
-            display(dbn, training_images[666]);
-            display(dbn, training_images[1024]);
+            display(dbn, dataset.training_images[256]);
+            display(dbn, dataset.training_images[512]);
+            display(dbn, dataset.training_images[666]);
+            display(dbn, dataset.training_images[1024]);
 
-            test_all(dbn, training_images, training_labels, dbn::predictor());
+            test_all(dbn, dataset, dbn::predictor());
         } else {
-            test_all(dbn, training_images, training_labels, dbn::predictor());
+            test_all(dbn, dataset, dbn::predictor());
         }
     }
 
