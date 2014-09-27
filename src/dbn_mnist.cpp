2df39866317b7a52b8eda12f6277b1f097bc9df0
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-09-27 19:53:49 +0200

    Merge two sample

--- src/dbn_mnist.cpp
+++ src/dbn_mnist.cpp
@@ -6,6 +6,7 @@
 //=======================================================================
 
 #include <iostream>
+#include <iomanip>
 #include <memory>
 
 #define DLL_SVM_SUPPORT
@@ -15,6 +16,8 @@
 
 #include "mnist/mnist_reader.hpp"
 #include "mnist/mnist_utils.hpp"
+
+#include "utils.hpp"
 
 namespace {
 
@@ -31,6 +34,87 @@ void test_all(DBN& dbn, Dataset& dataset, P&& predictor){
     std::cout << "\tError rate (normal): " << 100.0 * error_rate << std::endl;
 }
 
+template<typename DBN, typename Image>
+void display(const DBN& dbn, const Image& image){
+    auto weights = dbn->activation_probabilities(image);
+
+    for(std::size_t i = 0; i < 28; ++i){
+        for(std::size_t j = 0; j < 28; ++j){
+            std::cout << (image[i * 28 + j] * 255.0 > 10.0 ? 1.0 : 0.0) << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Activation probabilities" << std::endl;
+    for(std::size_t i = 0; i < 10; ++i){
+        auto w = weights[i];
+        std::cout << "\t" << i << ":" << w << "(" << w * 100.0 << "%)" << std::endl;
+    }
+    std::cout  << "Answer" << dbn->predict(image) << std::endl;
+}
+
+template<typename DBN, typename Images, typename Labels>
+void errors(const DBN& dbn, Images& images, Labels& labels){
+    std::size_t types[100];
+    std::size_t sources[10];
+    std::size_t errors = 0;
+    std::size_t second_errors = 0;
+
+    std::fill(&types[0], &types[100], 0);
+    std::fill(&sources[0], &sources[10], 0);
+
+    for(std::size_t i = 0; i < images.size(); ++i){
+        auto predicted = dbn->predict(images[i]);
+
+        if(predicted != labels[i]){
+            auto weights = dbn->activation_probabilities(images[i]);
+            auto max = 0.0;
+            auto second = 0;
+
+            for(std::size_t j = 0; j < weights.size(); ++j){
+                if(j != predicted && weights[j] > max){
+                    second = j;
+                    max = weights[j];
+                }
+            }
+
+            if(second == labels[i]){
+                ++second_errors;
+            }
+
+            ++errors;
+            ++types[labels[i] * 10 + predicted];
+            ++sources[labels[i]];
+        }
+    }
+
+    std::cout << "Error rate " << 100.0 * (static_cast<double>(errors) / images.size()) << std::endl;
+    std::cout << errors << " errors " << " / " << images.size() << std::endl;
+    std::cout << "Second guess error rate " << 100.0 * ((static_cast<double>(errors) - second_errors) / images.size()) << std::endl;
+    std::cout << "Second guess was right " << second_errors << " / " << images.size() << std::endl;
+
+    std::cout << "Errors sources: ";
+    for(std::size_t i = 0; i < 10; ++i){
+        std::cout << i << ":" << sources[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Error matrix" << std::endl;
+    std::cout << "   ";
+    for(std::size_t i = 0; i < 10; ++i){
+        std::cout << std::setw(3) << i << " ";
+    }
+    std::cout << std::endl;
+    for(std::size_t i = 0; i < 10; ++i){
+        std::cout << i << ": ";
+        for(std::size_t j = 0; j < 10; ++j){
+            std::cout << std::setw(3) << types[i * 10 + j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
 } //end of anonymous namespace
 
 int main(int argc, char* argv[]){
@@ -38,6 +122,8 @@ int main(int argc, char* argv[]){
     auto load = false;
     auto svm = false;
     auto grid = false;
+    auto gray = false;
+    auto prob = false;
 
     for(int i = 1; i < argc; ++i){
         std::string command(argv[i]);
@@ -57,6 +143,14 @@ int main(int argc, char* argv[]){
         if(command == "grid"){
             grid = true;
         }
+
+        if(command == "gray"){
+            gray = true;
+        }
+
+        if(command == "prob"){
+            prob = true;
+        }
     }
 
     auto dataset = mnist::read_dataset<std::vector, etl::dyn_vector, double>(100);
@@ -65,82 +159,145 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    mnist::binarize_dataset(dataset);
+    //Gray input
+    if(gray){
+        normalize(dataset.training_images);
+        normalize(dataset.test_images);
 
-    if(simple){
-        typedef dll::dbn_desc<
-            dll::dbn_layers<
+        if(simple){
+            typedef dll::dbn_desc<
+                dll::dbn_layers<
                 dll::rbm_desc<28 * 28, 100, dll::batch_size<50>, dll::init_weights, dll::momentum, dll::weight_decay<dll::decay_type::L2>>::rbm_t,
                 dll::rbm_desc<100, 100, dll::batch_size<50>, dll::momentum, dll::weight_decay<dll::decay_type::L2>>::rbm_t,
                 dll::rbm_desc<110, 200, dll::batch_size<50>, dll::momentum, dll::weight_decay<dll::decay_type::L2>>::rbm_t
-            >>::dbn_t dbn_simple_t;
+                    >>::dbn_t dbn_simple_t;
 
-        auto dbn = std::make_shared<dbn_simple_t>();
+            auto dbn = std::make_shared<dbn_simple_t>();
 
-        dbn->train_with_labels(dataset.training_images, dataset.training_labels, 10, 5);
+            dbn->train_with_labels(dataset.training_images, dataset.training_labels, 10, 5);
 
-        test_all(dbn, dataset, dll::label_predictor());
-    } else if(svm){
-        typedef dll::dbn_desc<
+            test_all(dbn, dataset, dll::label_predictor());
+        } else {
+            typedef dll::dbn_desc<
+                dll::dbn_layers<
+                dll::rbm_desc<28 * 28, 300, dll::momentum, dll::batch_size<100>, dll::init_weights, dll::visible<dll::unit_type::GAUSSIAN>>::rbm_t,
+                dll::rbm_desc<300, 500, dll::momentum, dll::batch_size<100>>::rbm_t,
+                dll::rbm_desc<500, 10, dll::momentum, dll::batch_size<100>, dll::hidden<dll::unit_type::SOFTMAX>>::rbm_t
+                    >>::dbn_t dbn_t;
+
+            auto dbn = std::make_unique<dbn_t>();
+
+            dbn->display();
+
+            if(load){
+                std::cout << "Load from file" << std::endl;
+
+                std::ifstream is("dbn_gray.dat", std::ifstream::binary);
+                dbn->load(is);
+            } else {
+                std::cout << "Start pretraining" << std::endl;
+                dbn->pretrain(dataset.training_images, 20);
+
+                std::cout << "Start fine-tuning" << std::endl;
+                dbn->fine_tune(dataset.training_images, dataset.training_labels, 2, 1000);
+
+                std::ofstream os("dbn_gray.dat", std::ofstream::binary);
+                dbn->store(os);
+            }
+
+            if(prob){
+                display(dbn, dataset.training_images[256]);
+                display(dbn, dataset.training_images[512]);
+                display(dbn, dataset.training_images[666]);
+                display(dbn, dataset.training_images[1024]);
+                display(dbn, dataset.training_images[2048]);
+
+                std::cout << std::endl << "Results on training dataset" << std::endl;
+                errors(dbn, dataset.training_images, dataset.training_labels);
+
+                std::cout << std::endl << "Results on test dataset" << std::endl;
+                errors(dbn, dataset.test_images, dataset.test_labels);
+            } else {
+                test_all(dbn, dataset, dll::predictor());
+            }
+        }
+    } else {
+        mnist::binarize_dataset(dataset);
+
+        if(simple){
+            typedef dll::dbn_desc<
+                dll::dbn_layers<
+                dll::rbm_desc<28 * 28, 100, dll::batch_size<50>, dll::init_weights, dll::momentum, dll::weight_decay<dll::decay_type::L2>>::rbm_t,
+                dll::rbm_desc<100, 100, dll::batch_size<50>, dll::momentum, dll::weight_decay<dll::decay_type::L2>>::rbm_t,
+                dll::rbm_desc<110, 200, dll::batch_size<50>, dll::momentum, dll::weight_decay<dll::decay_type::L2>>::rbm_t
+                    >>::dbn_t dbn_simple_t;
+
+            auto dbn = std::make_shared<dbn_simple_t>();
+
+            dbn->train_with_labels(dataset.training_images, dataset.training_labels, 10, 5);
+
+            test_all(dbn, dataset, dll::label_predictor());
+        } else if(svm){ typedef dll::dbn_desc<
             dll::dbn_layers<
                 dll::rbm_desc<28 * 28, 400, dll::momentum, dll::batch_size<50>, dll::init_weights>::rbm_t,
                 dll::rbm_desc<400, 600, dll::momentum, dll::batch_size<50>>::rbm_t
-        >>::dbn_t dbn_t;
+                    >>::dbn_t dbn_t;
 
-        auto dbn = make_unique<dbn_t>();
+            auto dbn = make_unique<dbn_t>();
 
-        dbn->display();
+            dbn->display();
 
-        if(load){
-            std::ifstream is("dbn.dat", std::ifstream::binary);
-            dbn->load(is);
-        } else {
-            dbn->pretrain(dataset.training_images, 20);
-
-            if(grid){
-                dbn->svm_grid_search(dataset.training_images, dataset.training_labels);
+            if(load){
+                std::ifstream is("dbn.dat", std::ifstream::binary);
+                dbn->load(is);
             } else {
-                if(!dbn->svm_train(dataset.training_images, dataset.training_labels)){
-                    std::cout << "SVM training failed" << std::endl;
+                dbn->pretrain(dataset.training_images, 20);
+
+                if(grid){
+                    dbn->svm_grid_search(dataset.training_images, dataset.training_labels);
+                } else {
+                    if(!dbn->svm_train(dataset.training_images, dataset.training_labels)){
+                        std::cout << "SVM training failed" << std::endl;
+                    }
                 }
+
+                std::ofstream os("dbn.dat", std::ofstream::binary);
+                dbn->store(os);
             }
 
-            std::ofstream os("dbn.dat", std::ofstream::binary);
-            dbn->store(os);
-        }
-
-        if(!grid){
-            test_all(dbn, dataset, dll::svm_predictor());
-        }
-    } else {
-        typedef dll::dbn_desc<
-            dll::dbn_layers<
+            if(!grid){
+                test_all(dbn, dataset, dll::svm_predictor());
+            }
+        } else {
+            typedef dll::dbn_desc<
+                dll::dbn_layers<
                 dll::rbm_desc<28 * 28, 100, dll::momentum, dll::batch_size<50>, dll::init_weights>::rbm_t,
                 dll::rbm_desc<100, 200, dll::momentum, dll::batch_size<50>>::rbm_t,
                 dll::rbm_desc<200, 10, dll::momentum, dll::batch_size<50>, dll::hidden<dll::unit_type::SOFTMAX>>::rbm_t
-        >, dll::watcher<dll::default_dbn_watcher>>::dbn_t dbn_t;
+                    >, dll::watcher<dll::default_dbn_watcher>>::dbn_t dbn_t;
 
-        auto dbn = make_unique<dbn_t>();
+            auto dbn = make_unique<dbn_t>();
 
-        dbn->display();
+            dbn->display();
 
-        if(load){
-            std::cout << "Load from file" << std::endl;
+            if(load){
+                std::cout << "Load from file" << std::endl;
 
-            std::ifstream is("dbn.dat", std::ifstream::binary);
-            dbn->load(is);
-        } else {
-            std::cout << "Start pretraining" << std::endl;
-            dbn->pretrain(dataset.training_images, 10);
+                std::ifstream is("dbn.dat", std::ifstream::binary);
+                dbn->load(is);
+            } else {
+                std::cout << "Start pretraining" << std::endl;
+                dbn->pretrain(dataset.training_images, 10);
 
-            std::cout << "Start fine-tuning" << std::endl;
-            dbn->fine_tune(dataset.training_images, dataset.training_labels, 5, 1000);
+                std::cout << "Start fine-tuning" << std::endl;
+                dbn->fine_tune(dataset.training_images, dataset.training_labels, 5, 1000);
 
-            std::ofstream os("dbn.dat", std::ofstream::binary);
-            dbn->store(os);
+                std::ofstream os("dbn.dat", std::ofstream::binary);
+                dbn->store(os);
+            }
+
+            test_all(dbn, dataset, dll::predictor());
         }
-
-        test_all(dbn, dataset, dll::predictor());
     }
 
     return 0;
