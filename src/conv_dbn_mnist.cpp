6322c6848421c4d234e43194e7818bb62c8a634b
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-09 15:04:41 +0200

    Tests on SVM with only one layer

--- src/conv_dbn_mnist.cpp
+++ src/conv_dbn_mnist.cpp
@@ -60,8 +60,8 @@ int main(int argc, char* argv[]){
     if(svm){
         typedef dll::conv_dbn_desc<
             dll::dbn_layers<
-            dll::conv_rbm_desc<28, 12, 40, dll::momentum, dll::batch_size<50>, dll::weight_decay<dll::decay_type::L2>, dll::sparsity<dll::sparsity_method::LEE>>::rbm_t,
-            dll::conv_rbm_desc<12, 6, 40, dll::momentum, dll::batch_size<50>, dll::weight_decay<dll::decay_type::L2>, dll::sparsity<dll::sparsity_method::LEE>>::rbm_t
+            dll::conv_rbm_desc<28, 17, 40, dll::momentum, dll::batch_size<50>, dll::weight_decay<dll::decay_type::L2>, dll::sparsity<dll::sparsity_method::LEE>>::rbm_t//,
+            //dll::conv_rbm_desc<12, 6, 40, dll::momentum, dll::batch_size<50>, dll::weight_decay<dll::decay_type::L2>, dll::sparsity<dll::sparsity_method::LEE>>::rbm_t
                 >>::dbn_t dbn_t;
 
         auto dbn = std::make_unique<dbn_t>();
@@ -69,11 +69,11 @@ int main(int argc, char* argv[]){
         dbn->layer<0>().pbias = 0.05;
         dbn->layer<0>().pbias_lambda = 100;
 
-        dbn->layer<1>().pbias = 0.05;
-        dbn->layer<1>().pbias_lambda = 100;
+        //dbn->layer<1>().pbias = 0.05;
+        //dbn->layer<1>().pbias_lambda = 100;
 
         std::cout << dbn->rbm_output<0>() << std::endl;
-        std::cout << dbn->rbm_input<1>() << std::endl;
+        //std::cout << dbn->rbm_input<1>() << std::endl;
 
         dbn->display();
 
@@ -84,15 +84,17 @@ int main(int argc, char* argv[]){
             dbn->load(is);
         } else {
             std::cout << "Start pretraining" << std::endl;
-            dbn->pretrain(dataset.training_images, 20);
+            dbn->pretrain(dataset.training_images, 100);
 
             if(grid){
                 svm::rbf_grid grid;
                 grid.type = svm::grid_search_type::LINEAR;
-                grid.c_first = 1.5;
-                grid.c_last = 10;
+                grid.c_first = 0.0;
+                grid.c_last = 20;
+                grid.c_steps = 25;
                 grid.gamma_first = 0;
                 grid.gamma_last = 200;
+                grid.gamma_steps = 12;
 
                 dbn->svm_grid_search(dataset.training_images, dataset.training_labels, 5, grid);
             } else {
